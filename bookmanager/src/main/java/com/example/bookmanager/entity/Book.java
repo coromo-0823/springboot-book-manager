833fa7a87ce239a9eedd061442ccdefb70badfec
coromo-0823 <coromo.0823@gmail.com>
2026-02-27 14:48:50 +0900

    Add Book entity

--- bookmanager/src/main/java/com/example/bookmanager/entity/Book.java
+++ bookmanager/src/main/java/com/example/bookmanager/entity/Book.java
@@ -1,16 +1,22 @@
 package com.example.bookmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     private String title;
     private String author;
-
-    // getter setter
 }
