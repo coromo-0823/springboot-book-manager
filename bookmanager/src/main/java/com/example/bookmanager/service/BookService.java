c419227a1dc35bdbfd64000f06f46e0509578acb
coromo-0823 <coromo.0823@gmail.com>
2026-02-28 04:55:34 +0900

    Add service

---
+++ bookmanager/src/main/java/com/example/bookmanager/service/BookService.java
@@ -0,0 +1,27 @@
+package com.example.bookmanager.service;
+
+import com.example.bookmanager.repository.BookRepository;
+import com.example.bookmanager.entity.Book;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+}
