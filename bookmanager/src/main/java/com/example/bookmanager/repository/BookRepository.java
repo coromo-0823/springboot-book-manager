e151a1fdb4c2efc31e7ac76b973415239e7545e2
coromo-0823 <coromo.0823@gmail.com>
2026-02-27 19:00:42 +0900

    Add repository

--- bookmanager/src/main/java/com/example/bookmanager/repository/BookRepository.java
+++ bookmanager/src/main/java/com/example/bookmanager/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.bookmanager.repository;
 
 import com.example.bookmanager.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
